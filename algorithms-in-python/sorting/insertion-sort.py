220e8ba56a987272b3a35ffb7a6c91db9d00a924
MarkNV <marknv1991@gmail.com>
2013-07-15 23:24:17 +0800

    finish insertion sort

--- algorithms-in-python/sorting/insertion-sort.py
+++ algorithms-in-python/sorting/insertion-sort.py
@@ -5,10 +5,25 @@
 
 import util
 
-def insertion_sort(array, ascend=True):
-    for i in len(array):
-        j = i-1
-        while array[i] < array[j]
 
-if __name__ == "main":
-    pass
+def insertion_sort(array, ascend=True):
+    for i in range(len(array)):
+        key = array[i]
+        j = i - 1
+        if ascend is True:
+            while key < array[j] and j >= 0:
+                array[j+1] = array[j]
+                j -= 1
+        else:
+            while key > array[j] and j >= 0:
+                array[j+1] = array[j]
+                j -= 1
+        array[j+1] = key
+    return array
+
+
+if __name__ == "__main__":
+    l = util.generate_array(15)
+    print l
+    array = insertion_sort(l, False)
+    print array
--- algorithms-in-python/sorting/util.py
+++ algorithms-in-python/sorting/util.py
@@ -3,12 +3,9 @@
 
 import random
 
-def generate_array(n, range):
+def generate_array(n, range=100):
     l = []
     while(n>0):
         l.append(random.randint(0,range))
         n -= 1
     return l
-
-l = generate_array(15, 200)
-print l 
