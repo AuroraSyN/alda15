d2edbc03d74e8829f73deeeb124d35565488d023
MarkNV <marknv1991@gmail.com>
2013-07-16 22:29:07 +0800

    rename test

---
+++ tests/test_insertion_sort.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from algorithm_in_python.sorting import utils
+from algorithm_in_python.sorting import insertion_sort
+from algorithm_in_python.sorting import merge_sort
+
+
+array = result = sort.utils.generate(201)
+sorted_array.sort()
--- tests/test_sort.py
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-import algorithm_in_python.sorting as sort
-
-
-array = sorted_array = sort.utils.generate(20)
-sorted_array.sort()
