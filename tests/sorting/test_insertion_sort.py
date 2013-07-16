62380b0cd8690c127003e9b99e10e29d302721d8
MarkNV <marknv1991@gmail.com>
2013-07-16 23:31:09 +0800

    add tests

---
+++ tests/sorting/test_insertion_sort.py
@@ -0,0 +1,12 @@
+#/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import data
+from algorithms_in_python.sorting import insertion_sort
+
+
+def test_insertion_sort():
+    a_result = insertion_sort.sort(data.array)
+    b_result = insertion_sort.sort(data.brray)
+    assert data.sorted_array == a_result
+    assert data.sorted_brray == b_result
