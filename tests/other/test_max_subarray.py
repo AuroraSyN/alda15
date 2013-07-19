81971c5a0847e24fb0ff799575b7eb50d3ce5909
MarkNV <marknv1991@gmail.com>
2013-07-19 22:51:33 +0800

    finish max array

---
+++ tests/other/test_max_subarray.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+from algorithms_in_python.other import max_subarray
+
+array = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+
+def test_max_subarray():
+    low, high, sum = max_subarray.find_max_subarray(array, 0, len(array) - 1)
+    assert low == 7
+    assert high == 10
+    assert sum == 43
