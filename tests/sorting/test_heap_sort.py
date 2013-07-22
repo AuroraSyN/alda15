680e7013096cbcd34de2b2fd11a66f78944cdc83
MarkNV <marknv1991@gmail.com>
2013-07-22 22:11:18 +0800

    update heap sort test

--- tests/sorting/test_heap_sort.py
+++ tests/sorting/test_heap_sort.py
@@ -2,11 +2,12 @@
 #-*- coding:utf-8 -*-
 
 
+import data
 from algorithms_in_python.sorting import heap_sort
 
 
-A = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-R = [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
-
 def test_heap_sort():
-    assert R == heap_sort.sort(A) 
+    a_result = heap_sort.sort(data.array)
+    b_result = heap_sort.sort(data.brray)
+    assert data.sorted_array == a_result
+    assert data.sorted_brray == b_result
