65cd9ab8c308e2bfbb67ebde54c3ab037b43dd72
MarkNV <marknv1991@gmail.com>
2013-07-24 13:39:04 +0800

    finish quick sort

---
+++ algorithms_in_python/sorting/quick_sort.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+# Quick Sort
+
+
+def sort(array, p, r):
+    if p < r:
+        q = partition(array, p, r)
+        sort(array, p, q - 1)
+        sort(array, q + 1, r)
+    return array
+
+
+def partition(array, p, r):
+    x = array[r]
+    i = p - 1
+    for j in range(p, r):
+        if array[j] <= x:
+            i += 1
+            tmp = array[i]
+            array[i] = array[j]
+            array[j] = tmp
+    tmp = array[i + 1]
+    array[i + 1] = array[r]
+    array[r] = tmp
+    return i + 1
---
+++ tests/sorting/test_quick_sort.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import data
+from algorithms_in_python.sorting import quick_sort
+
+
+def test_merge_sort():
+    a_result = quick_sort.sort(data.array, 0, len(data.array) - 1)
+    b_result = quick_sort.sort(data.brray, 0, len(data.brray) - 1)
+    assert a_result == data.sorted_array
+    assert b_result == data.sorted_brray
