7f21e62443b0ce1c7818326e26c728124415ea90
MarkNV <marknv1991@gmail.com>
2013-07-26 18:02:54 +0800

    finish Binary Search Tree

---
+++ algorithms_in_python/tree/binary_search_tree.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python2
+#-*- coding:utf-8 -*-
+
+# Binary Search Tree
+
+
+class bst:
+    """
+    BST node:
+      contains a left child, a right child and a data object
+    """
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        """
+        insert a node with data
+        """
+        # if the data object you want to insert is smaller than
+        # the data object held in current node
+        if data < self.data:
+            # if current node doesn't have a left child
+            if self.left is None:
+                # create a new node with the data then attach it
+                # to the current node as its left child
+                self.left = bst(data)
+            else:
+                #enter the current node's left sub-tree
+                self.left.insert(data)
+        # check may enter this part if input data
+        # is larger than the data in current node
+        else:
+            # similar check as in left sub-tree
+            if self.right is None:
+                self.right = bst(data)
+            else:
+                self.right.insert(data)
+
+    def delete(self, data):
+        """
+        Delete note with given data
+        """
+        node, parent = self.search(data)
+        if not node:
+            return None
+        else:
+            children_num = node.count_children()
+            # if node has no children, just remove it
+            if children_num == 0:
+                if parent.left is node:
+                    parent.left = None
+                else:
+                    parent.right = None
+                del node
+            # if node has one child, replace it by its child
+            elif children_num == 1:
+                if node.left:
+                    n = node.left
+                else:
+                    n = node.right
+                if parent.left is node:
+                    parent.left = n
+                if parent.right is node:
+                    parent.right = n
+                del node
+            # if node has 2 children
+            else:
+                # find its successor
+                successor = node.right
+                while successor.left:
+                    successor = successor.left
+                n, p = self.search(successor.data)
+                # replace node data with its successor's data
+                node.data = successor.data
+                # fix bst
+                if successor.right:
+                    node.right = successor.right 
+                if p is node:
+                    p.right = successor.right
+                    successor = None
+                elif successor.data < p.data:
+                    p.left = None
+                else:
+                    p.right = None
+
+    def search(self, data, parent=None):
+        """
+        Search data in given node
+        """
+        if data < self.data:
+            if self.left is None:
+                # return None to break searching
+                return None, None
+            # enter left sub-tree
+            return self.left.search(data, self)
+        elif data > self.data:
+            if self.right is None:
+                return None, None
+            # enter right sub-tree
+            return self.right.search(data, self)
+        # value matched
+        else:
+            return self, parent
+
+    def preorder_travel(self, output=[]):
+        """
+        travel tree pre-order
+        """
+        output.append(self.data)
+        if self.left:
+            self.left.preorder_travel(output)
+        if self.right:
+            self.right.preorder_travel(output)
+        return output
+
+    def inorder_travel(self, output=[]):
+        """
+        travel tree in-order
+        """
+        if self.left:
+            self.left.inorder_travel(output)
+        output.append(self.data)
+        if self.right:
+            self.right.inorder_travel(output)
+        return output
+
+    def postorder_travel(self, output=[]):
+        """
+        travel tree post-order
+        """
+        if self.left:
+            self.left.postorder_travel(output)
+        if self.right:
+            self.right.postorder_travel(output)
+        output.append(self.data)
+        return output
+
+    def compare(self, tree_node):
+        """
+        compare two tree
+        """
+        if not tree_node:
+            return False
+        if self.data != tree_node.data:
+            return False
+        flag = True
+        if not self.left:
+            if tree_node.left:
+                return False
+        else:
+            flag = self.left.compare(tree_node.left)
+        if not self.right:
+            if tree_node.right:
+                return False
+        else:
+            flag = self.right.compare(tree_node.right)
+        return flag
+
+    def count_children(self):
+        """
+        Return the number of children of the given node
+        """
+        count = 0
+        if self.left:
+            count += 1
+        if self.right:
+            count += 1
+        return count
+
+
+def build_tree(array):
+    """
+    build the binary search tree from array
+    """
+    root = bst(array.pop(0))
+    for item in array:
+        root.insert(item)
+    return root
---
+++ tests/tree/test_bst.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+
+from algorithms_in_python.tree.binary_search_tree import build_tree
+
+
+array = [8, 3, 10, 1, 6, 4, 7, 14, 13]
+
+pre_order = [8, 3, 1, 6, 4, 7, 10, 14, 13]
+in_order = [1, 3, 4, 6, 7, 8, 10, 13, 14]
+post_order = [1, 4, 7, 6, 3, 13, 14, 10, 8]
+
+
+def test_build_tree():
+    root = build_tree(array[:])
+    assert root.data == 8
+    assert root.left.data == 3
+    assert root.left.left.data == 1
+    assert root.left.right.data == 6
+    assert root.left.right.left.data == 4
+    assert root.left.right.right.data == 7
+    assert root.right.data == 10
+    assert root.right.right.data == 14
+    assert root.right.right.left.data == 13
+    assert root.left.left.left == None
+    assert root.left.left.right == None
+    assert root.left.right.left.left == None
+    assert root.left.right.left.right == None
+    assert root.left.right.right.left == None
+    assert root.left.right.right.right == None
+    assert root.right.left == None
+    assert root.right.right.right == None
+    assert root.right.right.left.left == None
+    assert root.right.right.left.right == None
+
+
+def test_preorder_travel():
+    root = build_tree(array[:])
+    assert pre_order == root.preorder_travel()
+
+
+def test_inorder_travel():
+    root = build_tree(array[:])
+    assert in_order == root.inorder_travel()
+
+
+def test_postorder_travel():
+    root = build_tree(array[:])
+    assert post_order == root.postorder_travel()
+
+
+def test_search():
+    root = build_tree(array[:])
+    node, parent = root.search(6)
+    assert node.left.data == 4
+    assert node.right.data == 7
+    assert parent.data == 3
+
+
+def test_compare():
+    tree1 = build_tree(array[:])
+    tree2 = build_tree(array[:])
+    tree3 = build_tree(post_order[:])
+    assert True == tree1.compare(tree2)
+    assert False == tree1.compare(tree3)
+
+
+def test_count_children():
+    root = build_tree(array[:])
+    node, parent = root.search(10)
+    assert 2 == root.count_children()
+    assert 1 == node.count_children()
+    assert 0 == node.right.left.count_children()
+
+
+def test_delete():
+    root = build_tree(array[:])
+    root.delete(1)
+    assert [8, 3, 6, 4, 7, 10, 14, 13] == root.preorder_travel([])
+    root = build_tree(array[:])
+    root.delete(14)
+    assert [8, 3, 1, 6, 4, 7, 10, 13] == root.preorder_travel([])
+    root = build_tree(array[:])
+    root.delete(3)
+    assert [8, 4, 1, 6, 7, 10, 14, 13] == root.preorder_travel([])
+    root = build_tree(array[:])
+    root.delete(6)
+    assert [8, 3, 1, 7, 4, 10, 14, 13] == root.preorder_travel([])
+    root = build_tree(array[:])
+    root.delete(8)
+    assert [10, 3, 1, 6, 4, 7, 14, 13] == root.preorder_travel([])
